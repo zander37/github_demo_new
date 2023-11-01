a76af0e195d1f5c0877d0c18923997c7d194312c
shaneallicock315 <149023319+shaneallicock315@users.noreply.github.com>
2023-10-31 22:26:09 -0400

    Update main2.py

--- main2.py
+++ main2.py
@@ -16,6 +16,22 @@ def encode(pswd):  # Encodes password.
         encoded_password += num
     return encoded_password
 
+def decode(pswd): # Decodes password
+  decoded_array = []
+  decoded_password = ""
+  for char in pswd:
+      if int(char) == 0:
+        decoded_array.append("7")
+      elif int(char) == 1:
+        decoded_array.append("8")
+      elif int(char) == 2:
+        decoded_array.append("9")
+      else:
+        decoded_array.append(str(int(char) - 3))
+
+  for num in decoded_array:
+    decoded_password += num
+  return decoded_password
 
 def main():  # Main Menu Function.
     while True:
@@ -26,7 +42,8 @@ def main():  # Main Menu Function.
             encoded = encode(password)
             print("Your password has been encoded and stored!\n")
         elif choice == 2:
-            continue
+            decoded = decode(encoded)
+            print(f"The encoded password is {encoded}, and the original password is {decoded}.\n")
         elif choice == 3:
             break
 
